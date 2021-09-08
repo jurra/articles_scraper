0884c29a8a493ead4dc8886262a2d683614c3954
jurra <jose.urra86@gmail.com>
2021-09-08 17:02:30 +0200

    First succesful setup of database and insert of items

--- modules/models.py
+++ modules/models.py
@@ -5,50 +5,62 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 article_author = Table(
-    'article_author', 
+    'article_author',
     Base.metadata,
-    Column('article_id', Integer, ForeignKey('article.id')),
-    Column('author_id', Integer, ForeignKey('author.id'))
+    Column('article_id', Integer, ForeignKey('article.id'), primary_key=True),
+    Column('author_id', Integer, ForeignKey('author.id'), primary_key=True)
 )
 
-collection_articles_collection = Table(
-    'collection_articles_collection',
-    Base.metadata,
-    Column('collection_id', Integer, ForeignKey('collection.id')),
-    Column('article_id', Integer, ForeignKey('article.id'))
-)
+# collection_articles_collection = Table(
+#     'collection_articles_collection',
+#     Base.metadata,
+#     Column('collection_id', Integer, ForeignKey('collection.id'), primary_key=True),
+#     Column('article_id', Integer, ForeignKey('article.id'), primary_key=True)
+# )
 
 article_publisher = Table(
     'article_publisher',
     Base.metadata,
-    Column('article_id', Integer, ForeignKey('article.id')),
-    Column('publisher_id', Integer, ForeignKey('publisher.id'))
+    Column('article_id', Integer, ForeignKey('article.id'), primary_key=True),
+    Column('publisher_id', Integer, ForeignKey('publisher.id'), primary_key=True)
 )
+
 
 class Article(Base):
     __tablename__ = 'article'
     id = Column(Integer, primary_key=True)
     title = Column(String)
-    datetime_created = Column(String)
-    datetime_updated = Column(String)
-    description = Column(String)
+    accessed_date = Column(String)
+    publishing_date = Column(String)
+    # description = Column(String)
     full_text = Column(String)
+    # publishing_date=Column(String)
     article_link = Column(String)
 
-    authors = relationship("Author", 
-                    secondary=article_author,   
-                    backref="article")
-                    
-    publishers = relationship("Publisher",backref="article")
+    authors = relationship("Author",
+                                secondary=article_author,
+                                back_populates="articles")
+
+    publishers = relationship("Publisher",
+                                secondary=article_publisher, 
+                                back_populates="articles")
+
+
 
 class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
-    article_id = Column(Integer, ForeignKey('article.id'))
+    # article_id = Column(Integer, ForeignKey('article.id'))
+    # publisher_id = Column(Integer, ForeignKey('publisher.id'))
 
-    articles = relationship("Article", backref="author")
+    articles = relationship("Article",
+                            secondary=article_author, 
+                            back_populates="authors")
+    # publishers = relationship("Publisher",
+    #                         secondary=article_publisher,
+    #                         back_populates="authors")
 
 
 class Publisher(Base):
@@ -56,17 +68,20 @@ class Publisher(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     url = Column(String)
-    article_id = Column(Integer, ForeignKey('article.id'))
+    articles = relationship("Article",
+                            secondary=article_publisher, 
+                            back_populates="publishers")
 
-    articles = relationship("Article", backref="publisher")
-    authors = relationship("Author", backref="publisher")
+    # authors = relationship("Author", 
+    #                         secondary=article_author,
+    #                         back_populates="publishers")
 
-class Collection(Base):
-    __tablename__ = 'collection'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    articles = relationship(
-        "Article",
-        secondary=collection_articles_collection,
-        backref="collection"
-    )
+
+# class Collection(Base):
+#     __tablename__ = 'collection'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     articles = relationship(
+#         "Article",
+#         secondary=collection_articles_collection,
+#         backref="collection")
--- tests/specs/test_specs.py
+++ tests/specs/test_specs.py
@@ -1,6 +1,50 @@
-def another_sum(a, b):
-    return a + b
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from modules.models import *
+# from modules.add_article import *
+import pandas
 
-def test_another_sum():
-    assert another_sum(3, 2) == 5
+from datetime import *
+
+from examples.my_articles_setup.config import config
+
+# Postgres username, password, and database name
+params = config()
+
+postgres_str = ('postgresql+psycopg2://{username}:{password}@{ipaddress}:{port}/{dbname}'
+.format(
+    username=params["user"],
+    password=params["password"],
+    ipaddress=params["host"],
+    dbname=params["database"],
+    port=5432)
+)
+
+# Create the connection
+cnx = create_engine(postgres_str)
+Base.metadata.bind = cnx
+Base.metadata.create_all(cnx)
+
+Session = sessionmaker(bind=cnx)        
+s = Session()  
+
+def test_add_article():
+    article = Article(
+    title="Example Title",
+    full_text="Example Text", 
+    article_link="http://example.com",
+    accessed_date=pandas.Timestamp("2018-01-01 00:00:00"),
+    publishing_date=pandas.Timestamp("2018-01-01 00:00:00"),)
+
+    # Get article from the databse
+    try:
+        article_from_db = s.query(Article).filter_by(title="Example Title").one()
+    except:
+        article_from_db = None
+    
+    assert article.title == article_from_db.title
+
+
+# close sessions
+s.close_all()
 
