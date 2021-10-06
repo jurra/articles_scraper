748bbf204a11aeb68371bd8c166292d693161f5c
jurra <jose.urra86@gmail.com>
2021-10-06 19:10:53 +0200

    Simple handling of duplicates

--- src/pipelines.py
+++ src/pipelines.py
@@ -25,7 +25,6 @@ class ArticlesPipeline(object):
         self.Session = sessionmaker(bind=engine)
         print("Connected successfully to database")
 
-        
     def process_item(self, item, spider):
         '''
         Saves articles to database
@@ -34,37 +33,35 @@ class ArticlesPipeline(object):
 
         # Create article object
         session = self.Session()
-        
         a = Article()
 
-        # If article.name is not in database, add the article
-        # if session.query(Article).filter(Article.title == item['title']).count() == 0:
-        a.title = item['title']
-        a.article_link = item['article_link']
-        a.full_text = item['full_text']
-        a.publishing_date = item['publishing_date']
+        try:
+            # If article.name is not in database, add the article
+            if session.query(Article).filter(Article.title == item['title']) is None:
+                a.title = item['title']
+                a.article_link = item['article_link']
+                a.full_text = item['full_text']
+                a.publishing_date = item['publishing_date']
 
-        # Create author if it doesnt exist in the database
-        au = Author()
-        au.name = item['author_name']
+                # Create author if it doesnt exist in the database
+                au = Author()
+                au.name = item['author_name']
 
+                # Check if author exists in database, if not, add the author
+                if session.query(Author).filter(Author.name == item['author_name']) is None:
+                    session.add(au)
+                    session.commit()
 
-        # Get the author name from the database
-        # au.name = item['author']
-        # query_author = session.query(Author).filter(Author.name == item['author_name']).one()
-        
-        # if query_author is None:
-        #     a.authors.append(au)
-        # else:
-        #     au.name = query_author.name
-        
-        # Add authors to article
-        a.authors.append(au)
-        
-        # Add article and author to session
-        session.add(au)
-        session.add(a)
-        session.commit()
+                # Append relationship between article and author
+                a.authors.append(au)
+
+                # Add article to database
+                session.add(a)
+                session.commit()
+                print("Article and properties sucessfully added")
+            
+            else:
+                print("Article already exists in database")
 
         # # Create collection object
         # if session.query(Collection).filter(Collection.name == item['collection']).count() == 0:
@@ -74,16 +71,14 @@ class ArticlesPipeline(object):
         #     )
         # else:
         #     co = session.query(Collection).filter(Collection.name == item['collection']).one()
-        
+
         # # Add collection to article
         # a.collections.append(co)
 
-        try:
-            print("Article and properties sucessfully added")
         except:
             session.rollback()
             print('Failed to add article')
-            raise 
+            raise
         finally:
             session.close()
 
